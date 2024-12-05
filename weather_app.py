c6ca843f9fa8ab0832f33abe7f44665a84c8eac7
Kesh Singh <keshwasya15@gmail.com>
2024-12-04 21:51:35 -0500

    add basic api call and parse data

--- weather_app.py
+++ weather_app.py
@@ -7,5 +7,24 @@ load_dotenv()
 
 
 api_key = os.getenv("API_KEY")
-city = "Ottawa"
+city = input("Enter city name: ")
 url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+
+response = requests.get(url)
+data = response.json()
+
+if data.get("cod") != 200:
+    print("City not found or invalid API key.")
+else:
+    temperature = data["main"]["temp"]
+    weather_description = data["weather"][0]["description"]
+    city_name = data["name"]
+
+    """Convert temp from Kelvin to Celsius"""
+    temperature_celsius = temperature - 273.15
+
+    print(f"City: {city_name}")
+    print(f"Temperature: {temperature_celsius:.2f}°C")
+    print(f"Weather: {weather_description}")
+    print("")
